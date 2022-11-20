2af3942bf76a44f5dd6f2dba362360e04ff71a89
Kevin Hakanson <kevhak@amazon.com>
2022-11-20 15:45:33 -0600

    add console.log

--- pages/ssr.js
+++ pages/ssr.js
@@ -13,7 +13,7 @@ export default function SSR({ formattedDate }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
   const renderDate = Date.now();
   const formattedDate = new Intl.DateTimeFormat("en-US", {
     dateStyle: "long",
@@ -22,5 +22,16 @@ export async function getServerSideProps() {
   console.log(
     `SSR ran on ${formattedDate}. This will be logged in CloudWatch.`
   );
+
+  // https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime
+  console.log(process.env); 
+
+  // https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props#context-parameter
+  console.log(context); 
+
+  // called from https://github.com/vercel/next.js/blob/canary/packages/next/server/render.tsx#L367
+  // var err = new Error();
+  // console.log(err.stack);
+
   return { props: { formattedDate } };
 }
